b225fd8fe53c95558634c1123d451bfb4b8ec1b4
Karthikeyan1028 <karthikidd03@gmail.com>
2024-07-25 22:53:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
-var res= fetch("https://restcountries.com/v3.1/all")
-res.then((data)=> {
-    return data.json();
-}).then((data1)=> foo(data1))
-.catch((error)=> console.log(error))
+async function fetchCountries() {
+    try {
+      let res = await fetch("https://restcountries.com/v3.1/all");
+      let countries = await res.json();
+      displayCountries(countries);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  async function fetchWeather(lat, lon) {
+    try {
+      let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ffddcf7e6d47a8129204fe2c582e3fc6`);
+      let weatherData = await res.json();
+      return weatherData.main.temp;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  function displayCountries(countries) {
+    let container = document.createElement("div");
+    container.className = "container";
+  
+    let row = document.createElement("div");
+    row.className = "row";
 
-var container=document.createElement("div");
-container.className="container";
-
-var row= document.createElement("div");
-row.className="row row-cols-lg-4  ";
-
-function foo(data1) {
-    for(var i=0;i<data1.length;i++) {
-        var col= document.createElement("div");
-        col.className= "col-lg-4"
-        col.innerHTML=` <div class="card h-100 mb-3" style="max-width:18rem;  ">
-                                      
-                                          <div class="card-header">${data1[i].name.common}</div>
-                                          <img src="${data1[i].flags?.png}" class="card-img-top">
+    countries.forEach((country,index) => {        var col= document.createElement("div");
+        col.className= "col-lg-4";
+        col.innerHTML=` <div class="card h-100 mb-3" style="max-width:20rem; ">                                 
+                                          <div class="card-header">${country.name.common}</div>
+                                          <img src="${country.flags?.png}" class="card-img-top">
                                           <div class="card-body">
-                                            <div class="card-text">Capital: ${data1[i].capital}</div>
-                                            <div class="card-text">Region: ${data1[i].region}</div>
-                                            <div class="card-text">Country Code: ${data1[i].cca3}</div>
-                                            <div class="card-text">Latitude: ${data1[i].latlng[0]}</div>
-                                            <div class="card-text">Longitude: ${data1[i].latlng[1]}</div>  
+                                            <div class="card-text">Capital: ${country.capital}</div>
+                                            <div class="card-text">Region: ${country.region}</div>
+                                            <div class="card-text">Country Code: ${country.cca3}</div>
+                                            <div class="card-text">Latitude: ${country.latlng[0]}</div>
+                                            <div class="card-text">Longitude: ${country.latlng[1]}</div>  
+                                            <button class="btn btn-primary" id="check-weather-${index}">Click for Weather</button>
                                           </div>
-                                      </div>
-                                      <br>`
-                                  
+                                      </div>`;
                                   row.append(col);
                                   container.append(row);
                                   document.body.append(container);
-                                           
-    }   
-    
+
+    let weather = document.getElementById(`check-weather-${index}`);
+    weather.addEventListener("click", async (event) => {
+      event.preventDefault();
+      let [lat, lon] = country.latlng;
+      let tempKelvin = await fetchWeather(lat, lon);
+      let tempCelsius = tempKelvin - 273.15;
+      let tempFahrenheit = (tempKelvin - 273.15)*(9/5)+32;
+      weather.textContent = `Temp: ${tempCelsius.toFixed(2)}°C/${tempFahrenheit.toFixed(2)}°F`;
+        
+    });  
+});
 }
+fetchCountries();
+    
